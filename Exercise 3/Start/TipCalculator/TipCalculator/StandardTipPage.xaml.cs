b65122711ec0e6bf10398e270cad015dcabf486d
prof79 <markus@markusegger.at>
2018-08-15 21:18:06 +0200

    Changed theme code to use ResourceDictionary but UI won't automatically update - yet.

--- Exercise 3/Start/TipCalculator/TipCalculator/StandardTipPage.xaml.cs
+++ Exercise 3/Start/TipCalculator/TipCalculator/StandardTipPage.xaml.cs
@@ -29,24 +29,36 @@ namespace TipCalculator
 
 		void OnLight(object sender, EventArgs e)
 		{
-			LayoutRoot.BackgroundColor = Color.Silver;
+            //LayoutRoot.BackgroundColor = Color.Silver;
 
-			tipLabel   .TextColor = Color.Navy;
-			billLabel  .TextColor = Color.Navy;
-			totalLabel .TextColor = Color.Navy;
-			tipOutput  .TextColor = Color.Navy;
-			totalOutput.TextColor = Color.Navy;
+            //tipLabel   .TextColor = Color.Navy;
+            //billLabel  .TextColor = Color.Navy;
+            //totalLabel .TextColor = Color.Navy;
+            //tipOutput  .TextColor = Color.Navy;
+            //totalOutput.TextColor = Color.Navy;
+
+            Resources["bgColor"] =
+                Color.FromHex("#c0c0c0");
+
+            Resources["fgColor"] =
+                Color.FromHex("#606060");
 		}
 
 		void OnDark(object sender, EventArgs e)
 		{
-			LayoutRoot.BackgroundColor = Color.Navy;
+            //LayoutRoot.BackgroundColor = Color.Navy;
 
-			tipLabel   .TextColor = Color.Silver;
-			billLabel  .TextColor = Color.Silver;
-			totalLabel .TextColor = Color.Silver;
-			tipOutput  .TextColor = Color.Silver;
-			totalOutput.TextColor = Color.Silver;
+            //tipLabel   .TextColor = Color.Silver;
+            //billLabel  .TextColor = Color.Silver;
+            //totalLabel .TextColor = Color.Silver;
+            //tipOutput  .TextColor = Color.Silver;
+            //totalOutput.TextColor = Color.Silver;
+
+            Resources["bgColor"] =
+                Color.FromHex("#606060");
+
+            Resources["fgColor"] =
+                Color.FromHex("#c0c0c0");
 		}
 
 		void GotoCustom(object sender, EventArgs e)
