c18f922bfa0e912b100714ef02d0596520f07a27
prof79 <markus@markusegger.at>
2018-08-15 21:29:54 +0200

    Imported project from exercise 3.

---
+++ Exercise 4/Start/TipCalculator/TipCalculator/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿// App.xaml.cs
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace TipCalculator
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new StandardTipPage());
+		}
+	}
+}
