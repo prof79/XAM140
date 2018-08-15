e5bbc5d400ba0437cb1801559b43dde2b56338f8
prof79 <markus@markusegger.at>
2018-08-15 21:39:49 +0200

    Imported project from exercise 4.

---
+++ Exercise 5/Start/TipCalculator/TipCalculator/StandardTipPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿// StandardTipPage.xaml.cs
+
+using System;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+	public partial class StandardTipPage : ContentPage
+	{
+		public StandardTipPage()
+		{
+			InitializeComponent();
+			billInput.TextChanged += (s, e) => CalculateTip();
+		}
+
+		void CalculateTip()
+		{
+			double bill;
+
+			if (Double.TryParse(billInput.Text, out bill) && bill > 0)
+			{
+				double tip   = Math.Round(bill*0.15, 2);
+				double final = bill + tip;
+
+				tipOutput  .Text = tip  .ToString("C");
+				totalOutput.Text = final.ToString("C");
+			}
+		}
+
+		void OnLight(object sender, EventArgs e)
+		{
+            //LayoutRoot.BackgroundColor = Color.Silver;
+
+            //tipLabel   .TextColor = Color.Navy;
+            //billLabel  .TextColor = Color.Navy;
+            //totalLabel .TextColor = Color.Navy;
+            //tipOutput  .TextColor = Color.Navy;
+            //totalOutput.TextColor = Color.Navy;
+
+            Resources["bgColor"] =
+                Color.FromHex("#c0c0c0");
+
+            Resources["fgColor"] =
+                Color.FromHex("#606060");
+		}
+
+		void OnDark(object sender, EventArgs e)
+		{
+            //LayoutRoot.BackgroundColor = Color.Navy;
+
+            //tipLabel   .TextColor = Color.Silver;
+            //billLabel  .TextColor = Color.Silver;
+            //totalLabel .TextColor = Color.Silver;
+            //tipOutput  .TextColor = Color.Silver;
+            //totalOutput.TextColor = Color.Silver;
+
+            Resources["bgColor"] =
+                Color.FromHex("#606060");
+
+            Resources["fgColor"] =
+                Color.FromHex("#c0c0c0");
+		}
+
+		void GotoCustom(object sender, EventArgs e)
+		{
+			Navigation.PushAsync(new CustomTipPage());
+		}
+	}
+}
